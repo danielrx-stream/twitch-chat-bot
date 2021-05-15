5b8f0b4214f858f8db8000e23f1d84b5d0df100c
DanielRX <16984675+DanielRX@users.noreply.github.com>
2021-05-15 10:30:34 +0100

    Slow down api polling

--- server/api-polling.ts
+++ server/api-polling.ts
@@ -61,11 +61,42 @@ export const run = async () => {
 
   const getSubCount = async() => {
     let data = await api.get('subscriptions', {version: ApiVersions.Helix, search: {broadcaster_id: to_id, first: countToGet}});
+    let allData = [...data.data];
     let count = data.data.length;
     while(data.data.length === countToGet && data.pagination && data.pagination.cursor) {
         data = await api.get('subscriptions', {version: ApiVersions.Helix, search: {broadcaster_id: to_id, first: countToGet, after: data.pagination.cursor}});
         count += data.data.length;
+        allData = [...allData, ...data.data]
     }
+    /*
+    allData = allData.map(removeTrash);
+    const gifts = allData.filter((x) => 'gifterName' in x);
+    const nonGifts = allData.filter((x) => !('gifterName' in x));
+    const gifters = gifts.map((x) => x.gifterName);
+    const collectGifts = (x: any[]) => {
+      const d: {[key: string]: any[]} = {};
+      gifters.forEach((gifter) => {
+        const g = x.filter((y) => y.gifterName === gifter);
+        if(g.length > 0) {
+          d[gifter] = g.map((x) => x.userName);
+        }
+      })
+      return d;
+    }
+    const removeTrash = ({tier, userName, gifterName, isGift}: any) => {
+      if(isGift === false) { return {tier, userName}; }
+      return {tier, userName, gifterName};
+    }
+    const getUsername =  ({userName}: any) => userName;
+
+    const giftsT1 = collectGifts(gifts.filter((x) => x.tier === "1000").map(getUsername));
+    const giftsT2 = collectGifts(gifts.filter((x) => x.tier === "2000").map(getUsername));
+    const giftsT3 = collectGifts(gifts.filter((x) => x.tier === "3000").map(getUsername));
+    const t1 = nonGifts.filter((x) => x.tier === "1000").map(getUsername);
+    const t2 = nonGifts.filter((x) => x.tier === "2000").map(getUsername);
+    const t3 = nonGifts.filter((x) => x.tier === "3000").map(getUsername);
+    await redis.hset(`streamData:${to_id}`, 'subsRaw', JSON.stringify({giftsT1, giftsT2, giftsT3, t1, t2, t3}));
+    */
     // console.log(`You have ${count} subs`);
     await redis.hset(`streamData:${to_id}`, 'subs', count - 1);
   }
@@ -92,4 +123,4 @@ export const run = async () => {
   return {getFollowCount, getSubCount, getCheerData, getVips, getMods};
 };
 run();
-setInterval(run, 30 * 1000);
+setInterval(run, 60 * 1000);
